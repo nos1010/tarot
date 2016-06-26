6c45a767443d35bc7b65ab4f3f2a570fabf5e43a
Cam Atkinson <cameronatkinson@camerons-macbook-air.local>
2016-06-26 14:50:40 +1000

    commit

--- Tarot/Modle.swift
+++ Tarot/Modle.swift
@@ -21,6 +21,36 @@ class Model
         majorArcanaCards = MajorArcanaCard.createDeck()
     }
     
+    // Returns the oracles response to the question posed by the user
+    func respond()->String
+    {
+        // Returns the Fool Card because this is the default
+        let cardFool = MajorArcanaCard()
+        let descFool = cardFool.getCardDescription()
+        var cardFoolImpersontor = cardFool
+        
+        // Returns the card I want based on the card number I provide
+        let cardMagician = MajorArcanaCard(number: 2)
+        
+        let descMagician = cardMagician?.getCardDescription()
+        
+        // Returns the card I want by its name as indicated by the Cases that define the card
+        let cardHermit = MajorArcanaCard.Hermit
+        
+        let cardRandom = MajorArcanaCard(number: Int(arc4random_uniform(UInt32(22))))
+        
+        let descRandom = cardRandom?.getCardDescription()
+        
+        // Returns a random integer within the range of indexes for the answers array
+        let response: Int = Int(arc4random_uniform(UInt32(MajorArcanaCard.getDeck().count)))
+        
+        // Set the current card name based on the specified index
+        currentCard = Array(MajorArcanaCard.getDeck())[response]
+        
+        return currentCard.interpretation
+    }
+    
+    /*
     // Return the oracles response to the question posed by the user
     func respond()->String
     {
@@ -30,7 +60,7 @@ class Model
         currentCard = Array(majorArcanaCards.values)[response]
         return currentCard.interpretation
     }
-    
+    */
     /*
     // Return the Oracle's response to the question posed by the user
     // using dictonary
